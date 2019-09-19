0493d2d1b3d472fe0a7fa1f1b38937a87004e7d1
Lambda_School_Loaner_112 <lambda_school_loaner_112@Lambda-School-Loaner-112s-MacBook-Pro.local>
2019-09-19 16:28:29 -0500

    completed

--- api/server.js
+++ api/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session')
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'chocochip', //sid
+  secret: process.env.SESSION_SECRET || ' keep it secret, keep it safe',
+  cookie: {
+    maxAge: 1000 * 60 , // in milliseconds
+    secure: false, //true means only send cookie over https
+    httpOnly: true, //true means JS has no access to the cookie
+  },
+  resave: false,
+  saveUninitialized: false, //GDPR laws against setting cookies automatically 
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
